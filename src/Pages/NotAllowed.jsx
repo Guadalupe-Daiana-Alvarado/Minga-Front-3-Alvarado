3557c5656e78fd289a0244a153f386602f68099e
sofia <m.sofia.a.campos@gmail.com>
2023-09-08 16:17:32 -0300

    front casi

---
+++ src/Pages/NotAllowed.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotAllowed = () => (
+    <div>
+      <h2>Error 403 - Acceso prohibido</h2>
+      <p>No tienes permiso para acceder a esta página.</p>
+    </div>
+  );
+
+export default NotAllowed
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,33 +1,41 @@
 import React from 'react'
 import {createBrowserRouter} from 'react-router-dom'
 import Index from '../Pages/Index'
-import Registre from '../Pages/Registre'
+import Register from '../Pages/Registre'
 import SignIn from '../Pages/SignIn'
 import Layout from '../Layouts/Layout'
 import Mangas from '../Pages/Mangas'
+import NotAllowed from '../Pages/NotAllowed'
+import Author from '../Pages/Author'
+
+const isAuthenticated = true; //
 
     const Router = createBrowserRouter([
        
        {
         path: "/",
-        element: <Layout/>,
+        element: <Layout isAuthenticated={isAuthenticated}/>,
         children: [
             {
                 path: '/',
                 element: <Index/>
             },
             {
-                path: '/Registre',
-                element: <Registre/>
+                path: '/Register',
+                element: <Register/>
             },
             {
                 path: '/SignIn',
                 element: <SignIn/>
             },
             {
-                path: '/Mangas',
-                element: <Mangas/>
+                path: '/Mangas/:page',
+                element: isAuthenticated ? <Mangas /> : <NotAllowed />
             },
+            {
+                path: 'authors/me',
+                element: <Author/>
+            }
         ]
        }
        
