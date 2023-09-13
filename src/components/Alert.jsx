62f6e9c238dacffddf39156e61c76c2e63064ac1
sofia <m.sofia.a.campos@gmail.com>
2023-09-13 18:17:44 -0300

    alert

---
+++ src/components/Alert.jsx
@@ -0,0 +1,45 @@
+
+import React from 'react';
+
+const Alert = ({ show, setShow, message }) => {
+  return (
+    <div className='flex h-screen justify-center items-center absolute'>
+      <div className='min-h-32 w-64 bg-white flex flex-col items-center justify-center rounded-lg'>
+        
+        
+        {message?message.map((errorMessage, index) => (
+          
+          <h1
+            key={index}
+            className='min-h-2/3 w-full text-center border-b-2 rounded-t-lg'
+          >
+            {errorMessage}
+          </h1>
+        )) : <h1 className='min-h-2/3 w-full text-center border-b-2 rounded-t-lg'>
+        
+        Created succesfully!
+      </h1>}
+        <button onClick={() => setShow(!show)} className='h-1/3 w-full rounded-b-lg text-blue-500'>
+          Accept
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Alert;
+
+// import React from 'react'
+
+// const Alert = ({show, setShow, message}) => {
+//   return (
+//     <div className='flex h-screen justify-center items-center absolute'>
+//       <div className='h-32 w-64 bg-white flex flex-col items-center justify-center rounded-lg'>
+//         {message.map((messages) => {`<h1 className='h-2/3 w-full text-center pt-5 border-b-2 rounded-t-lg'>${messages}</h1>`})}
+//         <button onClick={()=>setShow(!show)} className='h-1/3 w-full rounded-b-lg text-blue-500'>Accept</button>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Alert
