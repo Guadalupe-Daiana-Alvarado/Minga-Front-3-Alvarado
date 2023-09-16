8dccaabdfd8b5ecd2659e67968e249d654ba1faa
bellavergara <bellaiivanavergara@gmail.com>
2023-09-16 13:30:18 -0300

    cambios-finales

--- src/Pages/NotAllowed.jsx
+++ src/Pages/NotAllowed.jsx
@@ -2,14 +2,14 @@
 import React from 'react'
 import Carrousel from '../components/Carrousel'
 
- function NotAllowed() {
+function NotAllowed() {
   return (
     <>
-    <Carrousel />
+      <Carrousel />
       <main className='w-full h-96 bg-no-repeat bg-cover flex justify-around' style={{ backgroundImage: "url('./image/backgroundMain.png')" }}>
-      <div>
-        <h2 className='p-5 m-20 bg-white text-bold'>NotAllowed</h2>
-      </div>
+        <div>
+          <h2 className='p-5 m-20 bg-white text-bold'>NotAllowed</h2>
+        </div>
       </main>
     </>
   )
