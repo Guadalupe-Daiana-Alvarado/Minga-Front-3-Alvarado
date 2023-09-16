c9886a7aa1a2ecc46c9c790f88e4cfc9f892b902
bellavergara <bellaiivanavergara@gmail.com>
2023-09-16 15:55:39 -0300

    reparacion de redux y aplicando

--- src/redux/reducers/me_authors.js
+++ src/redux/reducers/me_authors.js
@@ -5,17 +5,16 @@ const initialState = {
   authors: []
 }
 // Se actualiza el estado
-const me_authors = createReducer(initialState, (builder) => 
+const me_authors = createReducer(initialState, (builder) =>
   builder
-    .addCase(authorData, (state, action) =>{
+    .addCase(authorData, (state, action) => {
       console.log(action);
       console.log(action.payload);
       const newState = {
-        ... state, // Este spread copia el estado inicial
+        ...state, // Este spread copia el estado inicial
         authors: []
       }
       return newState
-    } )
+    })
 )
-
 export default me_authors
